b93c37bf4027131ddfb1be1a195aa3f9a0d0c353
devanshhalwai <devansh.halwai@gmail.com>
2020-03-29 22:18:42 +0530

    3-sum-closest

--- 16.3-sum-closest.cpp
+++ 16.3-sum-closest.cpp
@@ -7,8 +7,26 @@
 // @lc code=start
 class Solution {
 public:
-    int threeSumClosest(vector<int>& nums, int target) {
-        
+    
+    int threeSumClosest(vector<int>& nums, int target) 
+    {
+        sort(nums.begin(), nums.end()); 
+        long long int closestSum = 9999999999;
+        for (long long int i = 0; i < nums.size() - 2; i++) 
+        { 
+            long long int ptr1=i+1,ptr2=nums.size()-1; 
+            while (ptr1<ptr2) 
+            {
+                long long int sum=nums[i]+nums[ptr1]+nums[ptr2]; 
+                if(abs(target-sum)<abs(target-closestSum)) 
+                    closestSum = sum; 
+                if(sum>target) 
+                    ptr2--; 
+                else 
+                    ptr1++;
+            } 
+        } 
+        return closestSum;
     }
 };
 // @lc code=end
